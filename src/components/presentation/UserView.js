2b11ae25ff4bc7f4bc14bdc0397f5a59cf7bd71c
Arun <palerdot@gmail.com>
2019-04-26 10:25:32 +0530

    [meditation] book 8, verse 49

--- src/components/presentation/UserView.js
+++ src/components/presentation/UserView.js
@@ -150,14 +150,27 @@ function MeditationsLink() {
   const meditations = [
     {
       id: 0,
-      text: 'hello',
+      text: 'porumai',
       meditation: `“If you suffer distress because of some external cause, it is not the thing itself that troubles you but your judgement about it, and it is within your power to cancel that judgement at any moment. But if what distresses you is something that lies in your own disposition, who is to prevent you from correcting your way of thinking? And likewise, if you are distressed because you are failing to accomplish some particular action which strikes you as sound, why do you not persist in the action rather than yield to the distress? ‘But something too strong for me is blocking my way.’ Then you should not be distressed, because the responsibility for your failure to act does not rest with you.—‘But my life is not worth living if this act is left undone.’—Then depart from life with generous feelings in your heart, dying in the same spirit as one who achieves his purpose, and reconciled, too, to what has stood in his way.”
+      
+      Book 8: verse 47
       `,
     },
     {
       id: 1,
-      text: 'universe',
-      meditation: `“Remember that your ruling centre becomes invincible when it withdraws into itself and rests content with itself, doing nothing other than what it wishes, even where its refusal to act is not reasonably based; and how much more contented it will be, then, when it founds its decision on reason and careful reflection. By virtue of this, an intelligence free from passions is a mighty citadel;* for man has no stronghold more secure to which he can retreat to remain unassailable from that time onward. One who has failed to see this is merely ignorant, but one who has seen it and fails to take refuge there is beyond the aid of fortune.”`,
+      text: 'patience',
+      meditation: `“Remember that your ruling centre becomes invincible when it withdraws into itself and rests content with itself, doing nothing other than what it wishes, even where its refusal to act is not reasonably based; and how much more contented it will be, then, when it founds its decision on reason and careful reflection. By virtue of this, an intelligence free from passions is a mighty citadel;* for man has no stronghold more secure to which he can retreat to remain unassailable from that time onward. One who has failed to see this is merely ignorant, but one who has seen it and fails to take refuge there is beyond the aid of fortune.”
+      
+      Book 8: verse 48
+      `,
+    },
+    {
+      id: 2,
+      text: 'amaidhi',
+      meditation: `“Say nothing more to yourself than what the first impressions* report. You have been told that some person is speaking ill of you? That is what you have been told: as to the further point, that he has harmed you, that you have not been told. I see that my little child is ill? I see just that; I do not see that his life is at risk. And so, in this way, always keep to first impressions and add nothing of your own from within, and then nothing bad will befall you. Or rather, add that you are well acquainted with everything that comes to pass in the world.”
+      
+      Book 8: verse 49
+      `,
     },
   ]
 
