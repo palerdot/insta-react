578a3dca5cf4c667a8fc83401eb95e672da30e88
Arun <palerdot@gmail.com>
2019-05-03 21:16:48 +0530

    meditations

--- src/components/presentation/FooterView.js
+++ src/components/presentation/FooterView.js
@@ -14,10 +14,24 @@ export default function Footer(props) {
 
   const footer_meditations = [
     {
-      id: 1,
       meditation: `“They kill you, cut you up, pursue you with curses.’ And how does that prevent your mind from remaining pure, balanced, temperate, and just? It is as if someone were standing by a sweet clear-flowing spring* and hurling curses at it: but for all that, it never stops brimming over with water good to drink, and if he throws mud into it, or dung, the spring will swiftly disperse it and wash it away, and suffer no defilement. How, then, are you to have an ever-flowing spring in you, and not a pool of standing water? If you hold firm to independence at every hour, along with kindness, simplicity, and reverence.”
       `,
       source: 'Book 8. Verse 51',
+    },
+    {
+      meditation: `“Accordingly, anyone who is not himself neutral towards pleasure and pain, or life and death, or reputation and disrepute, to which universal nature adopts a neutral attitude, commits a manifest impiety.”
+      `,
+      source: `Book 9. Verse 1`,
+    },
+    {
+      meditation: `“Blot out imagination; put a curb on impulse; quench desire; ensure that your ruling centre remains under its own control”
+      `,
+      source: `Book 9. Verse 7`,
+    },
+    {
+      meditation: `“Today I escaped the power of circumstance, or rather I cast all circumstance out; for it was not outside me, but within me, in my judgements.”
+      `,
+      source: `Book 9, Verse 13`,
     },
   ]
 
@@ -39,8 +53,8 @@ export default function Footer(props) {
       >
         <ModalHeader toggle={toggleModal}>Meditations</ModalHeader>
         <ModalBody>
-          {footer_meditations.map(m => (
-            <FooterMeditation key={m.id} {...m} />
+          {footer_meditations.map((m, index) => (
+            <FooterMeditation key={index} {...m} />
           ))}
         </ModalBody>
         <ModalFooter>
